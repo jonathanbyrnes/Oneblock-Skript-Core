fd1af16ee589baecbe1964d9b230dbd9803683c8
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-06-05 23:12:34 +0100

    fix: use vault placeholders to send new components

--- src/main/java/byrnes/jonathan/util/MessageUtil.java
+++ src/main/java/byrnes/jonathan/util/MessageUtil.java
@@ -2,9 +2,21 @@ package byrnes.jonathan.util;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class MessageUtil {
+
     public static Component color(String text) {
         return MiniMessage.miniMessage().deserialize(text != null ? text : "<red>Missing message.");
     }
+
+    public static Component legacyColor(String text) {
+        if (text == null || text.isEmpty()) return Component.empty();
+        return LegacyComponentSerializer.builder()
+                .character('&')
+                .hexColors()
+                .build()
+                .deserialize(text);
+    }
+
 }
